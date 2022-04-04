d82760cfdce735b7eeaa42044c62655e6e172ae3
lhall383 <lucashall383@gmail.com>
2022-04-03 22:06:24 -0500

    Implement Merge Overlapping Intervals

--- src/scripts/solve-coding-contracts.ts
+++ src/scripts/solve-coding-contracts.ts
@@ -1,22 +1,12 @@
-import { NS } from "@ns";
-
-function handleContract(ns: NS, filename: string, host: string): void {
-  const contractType = ns.codingcontract.getContractType(filename, host);
-  switch (contractType) {
-    case "Total Ways to Sum":
-      ns.print(ns.codingcontract.getData(filename, host));
-      ns.print(ns.codingcontract.getDescription(filename, host));
-      break;
-    default:
-      ns.print(`Unknown contract type '${contractType}'`);
-      break;
-  }
-}
+import { CodingAttemptOptions, NS } from "@ns";
 
 export async function main(ns: NS): Promise<void> {
+  ns.disableLog("ALL");
+  ns.print("----------Starting solve-coding-contracts----------");
+
   const fContents = ns.read("/data/flattened-list.txt");
   const serverList = JSON.parse(fContents).split(",") as string[];
-  ns.print(`Checking for CCTs on: ${serverList}`);
+  ns.print(`Checking for CCTs...`);
 
   for (const server of serverList) {
     if (!ns.serverExists(server)) continue;
@@ -24,11 +14,92 @@ export async function main(ns: NS): Promise<void> {
     const files = ns.ls(server, ".cct");
 
     if (files.length > 0) {
-      ns.print(`${server}: ${files}`);
-
       for (const f of files) {
-        handleContract(ns, f, server);
+        await handleContract(ns, f, server);
       }
     }
   }
+}
+
+async function handleContract(
+  ns: NS,
+  filename: string,
+  host: string
+): Promise<void> {
+  const contractType = ns.codingcontract.getContractType(filename, host);
+
+  let solution = [] as string[];
+  const data = ns.codingcontract.getData(filename, host);
+  const verbose = false;
+  switch (contractType) {
+    case "Merge Overlapping Intervals":
+      solution = [JSON.stringify(merge(ns, data as number[][], verbose))];
+      break;
+    case "Total Ways to Sum":
+      break;
+    default:
+      solution = [];
+      break;
+  }
+
+  if (solution && solution.length > 0) {
+    // submit attempt
+    const opts = { returnReward: true } as CodingAttemptOptions;
+    const result = ns.codingcontract.attempt(
+      solution,
+      filename,
+      host,
+      opts
+    ) as string;
+
+    // print output
+    const solved = result !== "";
+    const out = `${contractType} - ${solved}: ${JSON.stringify(
+      data
+    )} -> ${JSON.stringify(solution)}`;
+    ns.print(out);
+    ns.print(`${host} - ${filename} attempted. Rewards: ${result}`);
+
+    // write to file
+    await ns.write("/data/coding-contract-attempts.txt", out, "a");
+  } else {
+    ns.print(
+      `No solver for ${contractType}, not attempting ${host} ${filename}`
+    );
+  }
+}
+
+function merge(ns: NS, data: number[][], verbose = false): number[][] {
+  const clonedData = _.cloneDeep(data).sort((a, b) => a[0] - b[0]);
+  if (verbose) ns.print(clonedData);
+
+  let didCombine = false;
+  let input = _.cloneDeep(data).sort((a, b) => a[0] - b[0]);
+  let output = _.cloneDeep(data).sort((a, b) => a[0] - b[0]);
+  do {
+    didCombine = false;
+    input = _.cloneDeep(output);
+    output = [];
+    for (let i = 0; i < input.length - 1; i++) {
+      const current = input[i];
+      const next = input[i + 1];
+      if (current[1] >= next[0]) {
+        output.push([current[0], next[1]]);
+        didCombine = true;
+        i++;
+      } else {
+        output.push(current);
+        if (i === input.length - 2) output.push(input[i + 1]);
+      }
+    }
+
+    if (verbose)
+      ns.print(
+        `Input: ${JSON.stringify(input)}, Output: ${JSON.stringify(
+          output
+        )}, didCombine: ${didCombine}`
+      );
+  } while (didCombine && output.length > 1);
+
+  return output;
 }
